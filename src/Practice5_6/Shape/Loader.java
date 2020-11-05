1da83ea11b2554e2501ddb023e1930e1f5ce4f45
ilia <uhokozi@gmail.com>
2020-11-05 21:30:53 +0300

    Shape

--- src/Practice5_6/Shape/Loader.java
+++ src/Practice5_6/Shape/Loader.java
@@ -2,21 +2,21 @@ package Practice5_6.Shape;
 
 public class Loader {
     public static void main(String[] args) {
-        Circle circle_1 = new Circle(5, "Black", true);
-        circle_1.setRadius(6.5);
+        Circle circle_1 = new Circle(7, "Black", true);
+        circle_1.setRadius(14.5);
         circle_1.setColor("White");
         System.out.println("Радиус круга: " + circle_1.getRadius() + "; Цвет круга: " + circle_1.getColor());
         System.out.println("\n" + "Площадь круга: " + circle_1.getArea() + "; Периметр круга: " + circle_1.getPerimeter());
         System.out.println(circle_1.toString());
 
-        Rectangle rectangle_1 = new Rectangle(7.5, 11.2);
-        rectangle_1.setLength(7.5);
-        rectangle_1.setWidth(11.2);
+        Rectangle rectangle_1 = new Rectangle(9.5, 12.2);
+        rectangle_1.setLength(9.5);
+        rectangle_1.setWidth(12.2);
         System.out.println("Длина прямоугольника: " + rectangle_1.getLength() + "; Ширина прямоугольника: " + rectangle_1.getWidth());
         System.out.println("\n" + "Площадь прямоугольника: " + rectangle_1.getArea() + "; Периметр прямоугольника: " + rectangle_1.getPerimeter() + "\n");
         System.out.println(rectangle_1.toString());
 
-        Square square_1 = new Square(4.6);
+        Square square_1 = new Square(8.3);
         System.out.println("\n" + square_1.toString());
     }
 }
